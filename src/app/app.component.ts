3c020a282cf112c4f7d7322fdbb7486e0725c2f1
Filjok <filjofijothundiyil@gmail.com>
2022-11-20 20:22:45 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherData } from './models/weather.model';
+import { WeatherService } from './service/weather.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent implements OnInit{
+
+  constructor(private weatherService : WeatherService){
+
+  }
+
+  cityName:string='delhi';
+  weatherData?:WeatherData;
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName)
+    this.cityName=''
+  }
+private getWeatherData(cityName:string){
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next:(res)=>{
+        this.weatherData = res;
+        console.log(res);
+        
+      }
+    })
+
+    
+    
+  }
+
+  onsubmit(){
+    this.getWeatherData(this.cityName)
+    this.cityName=''
+
+  }
+}
+  
+ 
+    
+
+  
+
+
---
+++ src/app/service/weather.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from 'src/environments/environment';
+import { WeatherData } from '../models/weather.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http:HttpClient) { }
+
+
+
+  getWeatherData( cityName: any): Observable<WeatherData>{
+   return this.http.get<WeatherData>(environment.BaseUrl,{
+      headers: new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName,environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName,environment.XRapidAPIKeyHeaderValue),
+      params: new HttpParams()
+      .set('q',cityName)
+    })
+  }
+
+
+
+
+
+
+}
